17bbe6a572e1e7598fc5e949e90708fc39be981c
Gujex <txere@bk.ru>
2021-11-13 22:27:15 +0400

    Updates

---
+++ precache-manifest.77fe1e3e4b496afca72eab6360517728.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07d15d86b1eac3fad396af1fb19a65ea",
+    "url": "/Tours-app-react-js/index.html"
+  },
+  {
+    "revision": "7970304be9d9239a5475",
+    "url": "/Tours-app-react-js/static/css/main.8e49c9d7.chunk.css"
+  },
+  {
+    "revision": "303a7e45ea08ea333814",
+    "url": "/Tours-app-react-js/static/js/2.b79bb511.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tours-app-react-js/static/js/2.b79bb511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7970304be9d9239a5475",
+    "url": "/Tours-app-react-js/static/js/main.d84b5f2e.chunk.js"
+  },
+  {
+    "revision": "08321d530a943fc3a0f8",
+    "url": "/Tours-app-react-js/static/js/runtime-main.89cde199.js"
+  }
+]);
